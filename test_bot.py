56b844e8aa14e9c60a12dc9f3cff46fbfcbde8a4
Sheld1 <63401812+Sheld1@users.noreply.github.com>
2020-06-20 20:08:50 +0300

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -28,23 +28,23 @@ def welcome(message):
 
 pivo = ['"Essa Lime & Mint"']
 
-sti1 = open('ESSA lime&mint.webp', 'rb')
+p_description1 = 'Вкус: 2/10\nЦена: 45-60\nОпьянение: 8/10\nНе вкусное пиво для девочек.'
 
-vine = ['Может стоит попробовать красное сухое вино из ЮАР - "BARISTA PINOTAGE"?']
+vine = ['"BARISTA PINOTAGE"']
 
-sti2 = open('barista_pinotage.webp', 'rb')
+v_description1 = 'Вкус: 7/10\nЦена: 700(0.75л)\nОпьянение: 8/10\nМощный, с фруктово–ягодной кислотностью и сладкими джемово–ягодными тонами.'
 
 rum = ['"CAPTAIN MORGAN Original Spiced Gold"']
 
-sti3 = open('CAPTAIN MORGAN Original Spiced Gold.webp', 'rb')
+r_description1 = 'Вкус: 8.3/10\nЦена: 1100(0.7л)\nОпьянение: 8/10\nМягкий,с насыщенным вкусом, хорошо пьется,можно почти не закусывать.'
 
 cognac = ['"Коньяк армянский Старый КС Шалахо 10 лет"']
 
-sti4 = open('shalaho 10years.webp', 'rb')
+c_description1 = 'Вкус: 2.5/10\nЦена: 600(0.5л)\nОпьянение: 8.2/10\nТакой себе коньяк, на любителя, цена даже завышена.'
 
 whiskey = ['"Grant’s tripple wood"']
 
-sti5 = open('Grants-Whisky-Triple-Wood-bottle.webp', 'rb')
+w_description1 = 'Вкус: 7/10\nЦена: 1349(0.7л)\nОпьянение: 8/10\nГлубокий вкус, приятный запах, приемлемая цена.'
 
 choiser = ['Может стоит попробовать - ',
            'Я думаю стоит попробовать - ',
@@ -60,32 +60,98 @@ choiser = ['Может стоит попробовать - ',
 def get_text_messages(message):
     if message.chat.type == 'private':
         if message.text == '🍺Пиво':
+            global p
             p = random.choice(pivo)
-            bot.send_message(message.chat.id, random.choice(choiser) + p)
+            pivo_markup = types.InlineKeyboardMarkup(row_width=1)
+            item1 = types.InlineKeyboardButton('Описание', callback_data='description')
+            pivo_markup.add(item1)
+            bot.send_message(message.chat.id, random.choice(choiser) + p, reply_markup=pivo_markup)
+            sti1 = open('ESSA lime&mint.webp', 'rb')
             if p == '"Essa Lime & Mint"':
                 bot.send_sticker(message.chat.id, sti1)
         elif message.text == '🍷Вино':
+            global v
             v = random.choice(vine)
-            bot.send_message(message.chat.id, v)
-            if v == 'Может стоит попробовать красное сухое вино из ЮАР - "BARISTA PINOTAGE"?':
+            vine_markup = types.InlineKeyboardMarkup(row_width=1)
+            item1 = types.InlineKeyboardButton('Описание', callback_data='v_description')
+            vine_markup.add(item1)
+            bot.send_message(message.chat.id, random.choice(choiser) + v, reply_markup=vine_markup)
+            sti2 = open('barista_pinotage.webp', 'rb')
+            if v == '"BARISTA PINOTAGE"':
                 bot.send_sticker(message.chat.id, sti2)
         elif message.text == 'Ром':
+            global r
             r = random.choice(rum)
-            bot.send_message(message.chat.id, random.choice(choiser) + r)
+            rum_markup = types.InlineKeyboardMarkup(row_width=1)
+            item1 = types.InlineKeyboardButton('Описание', callback_data='r_description')
+            rum_markup.add(item1)
+            bot.send_message(message.chat.id, random.choice(choiser) + r, reply_markup=rum_markup)
+            sti3 = open('CAPTAIN MORGAN Original Spiced Gold.webp', 'rb')
             if r == '"CAPTAIN MORGAN Original Spiced Gold"':
                 bot.send_sticker(message.chat.id, sti3)
         elif message.text == 'Коньяк':
+            global c
             c = random.choice(cognac)
-            bot.send_message(message.chat.id, random.choice(choiser) + c)
+            cognac_markup = types.InlineKeyboardMarkup(row_width=1)
+            item1 = types.InlineKeyboardButton('Описание', callback_data='c_description')
+            cognac_markup.add(item1)
+            bot.send_message(message.chat.id, random.choice(choiser) + c, reply_markup=cognac_markup)
+            sti4 = open('shalaho 10years.webp', 'rb')
             if c == '"Коньяк армянский Старый КС Шалахо 10 лет"':
                 bot.send_sticker(message.chat.id, sti4)
         elif message.text == 'Виски':
+            global w
             w = random.choice(whiskey)
-            bot.send_message(message.chat.id, random.choice(choiser) + w)
+            whiskey_markup = types.InlineKeyboardMarkup(row_width=1)
+            item1 = types.InlineKeyboardButton('Описание', callback_data='w_description')
+            whiskey_markup.add(item1)
+            bot.send_message(message.chat.id, random.choice(choiser) + w, reply_markup=whiskey_markup)
+            sti5 = open('Grants-Whisky-Triple-Wood-bottle.webp', 'rb')
             if w == '"Grant’s tripple wood"':
                 bot.send_sticker(message.chat.id, sti5)
         else:
             bot.send_message(message.chat.id, 'Я не знаю что на это ответить 😥')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'description':
+                if p == '"Essa Lime & Mint"':
+                    bot.send_message(call.message.chat.id, p_description1)
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                          text=p,
+                                          reply_markup=None)
+                # elif p == ...
+            elif call.data == 'v_description':
+                if v == '"BARISTA PINOTAGE"':
+                    bot.send_message(call.message.chat.id, v_description1)
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                          text=v,
+                                          reply_markup=None)
+            elif call.data == 'r_description':
+                if r == '"CAPTAIN MORGAN Original Spiced Gold"':
+                    bot.send_message(call.message.chat.id, r_description1)
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                          text=r,
+                                          reply_markup=None)
+            elif call.data == 'c_description':
+                if c == '"Коньяк армянский Старый КС Шалахо 10 лет"':
+                    bot.send_message(call.message.chat.id, c_description1)
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                          text=c,
+                                          reply_markup=None)
+            elif call.data == 'w_description':
+                if w == '"Grant’s tripple wood"':
+                    bot.send_message(call.message.chat.id, w_description1)
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                          text=w,
+                                          reply_markup=None)
+
+
+    except Exception as e:
+        print(repr(e))
 
 
 bot.polling(none_stop=True)
